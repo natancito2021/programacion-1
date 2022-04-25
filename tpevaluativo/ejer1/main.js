dd43cd05aa096ba059614aff8dbcba4e8578ca4e
natancito2021 <rodrigueznatan763@gmail.com>
2022-04-25 17:07:31 -0300

    continuar

---
+++ tpevaluativo/ejer1/main.js
@@ -0,0 +1,18 @@
+function auto() {
+    const marca = document.getElementById("inp_a").value
+
+    if (marca == 'ford fiesta') {
+        const resultado = 1950000 - ((5 * 1950000) / 100)
+        alert("descuento:" + resultado)
+
+    } else {
+        if (marca == 'ford focus') {
+            const resultado = $2560000 - ((10 * 2560000) / 100)
+            alert("descuento:" + resultado)
+        }
+        else {
+            const resultado = $1950000 - ((15 * 1950000) / 100)
+            alert("descuento:" + resultado)
+        }
+    }
+}
